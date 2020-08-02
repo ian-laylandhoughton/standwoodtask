2972300ef7562082d9e8335f18798061d2c85fce
Ian Layland-Houghton <ianlh@apadmi.com>
2020-08-02 16:16:10 +0100

    Added repo detail view controller and UI. Added assets for detail screen.

---
+++ StandwoodTask/StandwoodTask/Repositories/ViewControllers/RepositoryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RepositoryDetailViewController.swift
+//  StandwoodTask
+//
+//  Created by Ian Layland-Houghton on 02/08/2020.
+//  Copyright © 2020 Ian Layland-Houghton. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
